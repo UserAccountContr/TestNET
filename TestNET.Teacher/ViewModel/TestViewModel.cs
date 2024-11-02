2da5425efdf9853bd15fdff8ae68dd1e77934ad5
UserAccountContr <icetolitos@gmail.com>
2024-11-03 00:39:40 +0200

    show

--- TestNET.Teacher/ViewModel/TestViewModel.cs
+++ TestNET.Teacher/ViewModel/TestViewModel.cs
@@ -1,53 +1,61 @@
-﻿using TestNET.Teacher.Service;
+﻿using System.Xml.Linq;
+using TestNET.Teacher.Service;
 
 namespace TestNET.Teacher.ViewModel;
 
 public partial class TestViewModel : BaseViewModel
 {
-    public ObservableCollection<Test> Tests { get; } = new();
-    TestService testService;
+	public ObservableCollection<Test> Tests { get; } = new();
+	TestService testService;
 
-    public TestViewModel()
-    {
-        this.testService = new TestService();
+	public TestViewModel()
+	{
+		this.testService = new TestService();
+	}
+
+	[RelayCommand]
+	void NewTest()
+	{
+		Tests.Add(new Test
+		{
+			Name = "Test1",
+			Questions = new List<Question>()
+			{
+				new Question
+				{
+					Text = "Q1",
+					Answer = "A1"
+				}
+			}
+		});
+	}
+
+	[RelayCommand]
+	void RemoveTest(object selitem)
+	{
+		Tests.Remove(selitem as Test);
+	}
+
+	[RelayCommand]
+	void SaveTest()
+	{
+		testService.SaveTests(Tests.ToList());
+	}
+
+	[RelayCommand]
+	async void LoadTest()
+	{
+		Tests.Clear();
+		var tests = await testService.GetTests();
+		foreach (var test in tests)
+		{
+			Tests.Add(test);
+		}
     }
 
     [RelayCommand]
-    void NewTest()
+    void ShowTest(object test)
     {
-        Tests.Add(new Test
-        {
-            Name = "Test1",
-            Questions = new List<Question>()
-            {
-                new Question
-                {
-                    Text = "Q1",
-                    Answer = "A1"
-                }
-            }
-        });
-    }
-
-    [RelayCommand]
-    void RemoveTest(object selitem)
-    {
-        Tests.Remove(selitem as Test);
-    }
-
-    [RelayCommand]
-    void SaveTest()
-    {
-        testService.SaveTests(Tests.ToList());
-    }
-
-    [RelayCommand]
-    async void LoadTest()
-    {
-        var tests = await testService.GetTests();
-        foreach (var test in tests)
-        {
-            Tests.Add(test);
-        }
+        MessageBox.Show($"Q: {(test as Test).Questions[0].Text}\nA: {(test as Test).Questions[0].Answer}", (test as Test).Name);
     }
 }
