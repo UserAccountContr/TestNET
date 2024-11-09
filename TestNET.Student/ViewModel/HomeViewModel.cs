e2b3a459028b8fccaf5dfecc66d48b298c97fcf6
UserAccountContr <icetolitos@gmail.com>
2024-11-09 19:02:32 +0200

    btn disabled

--- TestNET.Student/ViewModel/HomeViewModel.cs
+++ TestNET.Student/ViewModel/HomeViewModel.cs
@@ -9,16 +9,21 @@ public partial class HomeViewModel : BaseViewModel
     //
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(FullName))]
+    [NotifyPropertyChangedFor(nameof(CanStart))]
     string firstName;
 
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(FullName))]
+    [NotifyPropertyChangedFor(nameof(CanStart))]
     string lastName;
 
-    public string FullName { get => $"{FirstName + LastName}"; }
+    public string FullName { get => $"{FirstName} {LastName}"; }
 
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(CanStart))]
     string code;
+
+    public bool CanStart { get => !string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName) && !string.IsNullOrEmpty(Code); }
 
     //TestService service;
 
