89334247a28913419b95c8d4fb006a38065088ed
UserAccountContr <icetolitos@gmail.com>
2024-11-12 19:57:42 +0200

    - nenuzhni using

--- TestNET.Student/View/TestSolvingView.xaml.cs
+++ TestNET.Student/View/TestSolvingView.xaml.cs
@@ -1,28 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿namespace TestNET.Student.View;
 
-namespace TestNET.Student.View
+public partial class TestSolvingView : UserControl
 {
-    /// <summary>
-    /// Interaction logic for TestSolvingView.xaml
-    /// </summary>
-    public partial class TestSolvingView : UserControl
+    public TestSolvingView()
     {
-        public TestSolvingView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
