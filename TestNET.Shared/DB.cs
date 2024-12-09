d3d561b91adf1ebbb4482dab4b1a4a346982c7ba
eeyoretheman <yordan.iliev.todorov@gmail.com>
2024-12-09 17:40:56 +0200

    feat: start working on sqlite utilities

---
+++ TestNET.Shared/DB.cs
@@ -0,0 +1,22 @@
+﻿namespace TestNET.Shared;
+
+using Microsoft.Data.Sqlite;
+
+public static class DB
+{
+    public static void Initialize(string dbName)
+    {
+        using (var connection = new SqliteConnection($"Data Source={dbName}"))
+        {
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = @"
+    CREATE TABLE IF NOT EXISTS Users (
+                        
+                    )
+                ";
+            command.ExecuteNonQuery();
+        }
+    }
+}
