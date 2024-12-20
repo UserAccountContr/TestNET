93df83233841c03a62be8292b8090469fe4b6823
UserAccountContr <icetolitos@gmail.com>
2024-12-20 17:42:44 +0200

    removed unnecessary things

--- TestNET.Student/Service/Converter.cs
+++ TestNET.Student/Service/Converter.cs
@@ -15,24 +15,3 @@ public class MultiBindConverter : IMultiValueConverter
         throw new NotImplementedException();
     }
 }
-
-public class IndexConverter : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        if (value is FrameworkElement item)
-        {
-            ItemsControl itemsControl = ItemsControl.ItemsControlFromItemContainer(item);
-            int index = itemsControl.ItemContainerGenerator.IndexFromContainer(item);
-            return (index + 1).ToString();
-            // Adjust index as needed (e.g., start from 1)
-        }
-        return null;
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-
-    }
-}
