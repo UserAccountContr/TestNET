33069744e8831b62295b366e28dbe442cd38c410
UserAccountContr <icetolitos@gmail.com>
2025-01-15 12:29:41 +0200

    tochki na vuprosite (mc many)

--- TestNET.Shared/Model/QuestionMC.cs
+++ TestNET.Shared/Model/QuestionMC.cs
@@ -51,14 +51,23 @@ public partial class MultipleChoiceManyQuestion : Question, IManyAnswers
 
     public float Grade(ObservableCollection<Answer> submAnswers)
     {
+        int actuallycorrect = PossibleAnswers.Where(x => x.IsCorrect).Count();
+        int userthinksarecorrect = submAnswers.Where(x => x.IsCorrect).Count();
+
         float p = 0;
-        float perQ = Points / PossibleAnswers.Where(x => x.IsCorrect).ToList().Count;
+
+        float perQ = Points / actuallycorrect;
+
         foreach (Answer a in PossibleAnswers)
         {
-            if (a.IsCorrect && a.IsCorrect == submAnswers.Where(x => x.Text == a.Text).First().IsCorrect)
+            if (a.IsCorrect && submAnswers.Where(x => x.Text == a.Text).First().IsCorrect)
                 p += perQ;
+
+            if (actuallycorrect < submAnswers.Where(x=> x.IsCorrect).Count())
+                if (!a.IsCorrect && submAnswers.Where(x => x.Text == a.Text).First().IsCorrect)
+                    p -= perQ;
         }
-        return p;
+        return Math.Max(p, 0);
     }
 
     [RelayCommand]
