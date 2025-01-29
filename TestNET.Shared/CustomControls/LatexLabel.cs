123245e7447699d4a920894fd120bdac24bf2311
UserAccountContr <icetolitos@gmail.com>
2025-01-29 17:46:05 +0200

    custom controls

---
+++ TestNET.Shared/CustomControls/LatexLabel.cs
@@ -0,0 +1,72 @@
+﻿using WpfMath.Controls;
+
+namespace TestNET.Shared.CustomControls;
+
+[TemplatePart(Name = TEXT_DISPLAY_PART_NAME, Type = typeof(Label))]
+public class LatexLabel : ContentControl
+{
+    private const string TEXT_DISPLAY_PART_NAME = "PART_TextDisplay";
+
+    static LatexLabel()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(LatexLabel), new FrameworkPropertyMetadata(typeof(LatexLabel)));
+    }
+
+    public static readonly DependencyProperty IsMathProperty =
+       DependencyProperty.Register("IsMath", typeof(bool), typeof(LatexLabel),
+           new PropertyMetadata(false, OnIsMathPropertyChanged));
+
+    public bool IsMath
+    {
+        get { return (bool)GetValue(IsMathProperty); }
+        set { SetValue(IsMathProperty, value); }
+    }
+
+    public static readonly DependencyProperty TextProperty =
+       DependencyProperty.Register("Text", typeof(string), typeof(LatexLabel),
+           new PropertyMetadata("", OnTextPropertyChanged));
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    private static void OnIsMathPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is LatexLabel ll)
+        {
+            ll.OnIsMathPropertyChanged();
+        }
+    }
+    private void OnIsMathPropertyChanged()
+    {
+        UpdateContent();
+    }
+
+    private static void OnTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is LatexLabel ll)
+        {
+            ll.OnTextPropertyChanged();
+        }
+    }
+    private void OnTextPropertyChanged()
+    {
+        UpdateContent();
+    }
+
+    private void UpdateContent()
+    {
+        if (IsMath)
+        {
+            var temp = new FormulaControl();
+            temp.Formula = Text;
+            Content = temp;
+        }
+        else
+        {
+            Content = Text;
+        }
+    }
+}
---
+++ TestNET.Shared/CustomControls/QuestionPanel.cs
@@ -0,0 +1,29 @@
+﻿namespace TestNET.Shared.CustomControls;
+
+public class QuestionPanel : ContentControl
+{
+    static QuestionPanel()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(QuestionPanel), new FrameworkPropertyMetadata(typeof(QuestionPanel)));
+    }
+
+    public static readonly DependencyProperty QuestionIndexProperty =
+       DependencyProperty.Register("QuestionIndex", typeof(int), typeof(QuestionPanel),
+           new PropertyMetadata(0));
+
+    public int QuestionIndex
+    {
+        get { return (int)GetValue(QuestionIndexProperty); }
+        set { SetValue(QuestionIndexProperty, value); }
+    }
+
+    public static readonly DependencyProperty PointsProperty =
+       DependencyProperty.Register("Points", typeof(float), typeof(QuestionPanel),
+           new PropertyMetadata((float)0));
+
+    public float Points
+    {
+        get { return (float)GetValue(PointsProperty); }
+        set { SetValue(PointsProperty, value); }
+    }
+}
