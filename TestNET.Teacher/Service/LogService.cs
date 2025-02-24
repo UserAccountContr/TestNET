f7929ddae440668754eabe684000ab61650eeb4d
UserAccountContr <icetolitos@gmail.com>
2025-02-24 22:43:31 +0200

    nekvi pulni gluposti

--- TestNET.Teacher/Service/LogService.cs
+++ TestNET.Teacher/Service/LogService.cs
@@ -4,4 +4,13 @@ public partial class LogService : ObservableObject
 {
     [ObservableProperty]
     string testLog;
+
+    [ObservableProperty]
+    string iPCode;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(TestNotStarted))]
+    bool testStarted = false;
+
+    public bool TestNotStarted => !TestStarted;
 }
