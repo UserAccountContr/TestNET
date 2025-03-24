a39aa3e2817c60e56336b10ef6fc945d9c393c6f
UserAccountContr <icetolitos@gmail.com>
2025-03-24 15:39:04 +0200

    neshta

--- TestNET.Avalonia.Teacher/ViewModels/HomeViewModel.cs
+++ TestNET.Avalonia.Teacher/ViewModels/HomeViewModel.cs
@@ -1,4 +1,7 @@
-﻿using TestNET.Avalonia.Teacher.Service;
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
+using TestNET.Avalonia.Teacher.Service;
+
 namespace TestNET.Avalonia.Teacher.ViewModels;
 
 public partial class HomeViewModel : BaseViewModel
@@ -23,7 +26,7 @@ public partial class HomeViewModel : BaseViewModel
     void NewTest()
     {
         Tests.Add(new TeacherTest("New test", new(), new(), false));
-        //Navigation.NavigateTo<EditTestViewModel, Test>(Tests[^1]);
+        Navigation.NavigateTo<EditTestViewModel, Test>(Tests[^1]);
     }
 
     [RelayCommand]
@@ -33,22 +36,55 @@ public partial class HomeViewModel : BaseViewModel
         testService.DeleteTest((TeacherTest)selitem);
     }
 
-//    [RelayCommand]
-//    void ImportTest() 
-//    {
-//        OpenFileDialog openFileDialog = new OpenFileDialog();
-//        openFileDialog.Filter = "Test Database Files | *.db";
+    [RelayCommand]
+    async Task ImportTest() 
+    {
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime  desktopLifetime ?  desktopLifetime.MainWindow : null);
+
+        // Start async operation to open the dialog.
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open Text File",
+            FileTypeFilter = new[] { new FilePickerFileType("Test Database Files")
+            {
+                Patterns = new[] { "*.db" },
+                //AppleUniformTypeIdentifiers = new[] { } ,
+                //MimeTypes = new[] { "image/*" }
+            } },
+            AllowMultiple = false
+        });
+
+        if (files.Count >= 1)
+        {
+            //// Open reading stream from the first file.
+            //await using var stream = await files[0].OpenReadAsync();
+            //using var streamReader = new StreamReader(stream);
+            //// Reads all the content of file as a text.
+            //var fileContent = await streamReader.ReadToEndAsync();
+            
+            var test = testService.ImportTest(files[0].Path.ToString());
+            
+            if (test != null)
+            {
+                Tests.Add(test);
+            }
+        }
+        
+        
+        //OpenFileDialog openFileDialog = new OpenFileDialog();
+        //openFileDialog.Filter = "Test Database Files | *.db";
+
+        //if (openFileDialog.ShowDialog() == true)
+        //{
+        //    var test = testService.ImportTest(openFileDialog.FileName);
 //
-//        if (openFileDialog.ShowDialog() == true)
-//        {
-//            var test = testService.ImportTest(openFileDialog.FileName);
-//
-//            if (test != null)
-//            {
-//                Tests.Add(test);
-//            }
-//        }
-//    }
+        //    if (test != null)
+        //    {
+        //        Tests.Add(test);
+        //    }
+        //}
+    }
 
     [RelayCommand]
     void SaveTest() => testService.SaveTests(Tests.ToList());
